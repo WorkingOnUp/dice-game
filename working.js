f95ff436b2be526a7bee6b50b8724641792a9143
Anna Chacko <32591437+WorkingOnUp@users.noreply.github.com>
2019-06-15 00:54:55 +0530

    Complete

--- working.js
+++ working.js
@@ -1,10 +1,10 @@
-var face1,face2,flag=0,point1,point2,opoint1=0,opoint2=0;
+var face1,face2,flag=0,point1=0,point2=0,opoint1=0,opoint2=0;
         const initialRender = function () {
             const initText1 = document.createElement('h1');
             initText1.textContent = 'Press the button to begin';
             document.querySelector('.textOneBoard').appendChild(initText1);
             const initText2 = document.createElement('h1');
-            initText2.textContent = 'Press the button to begin';
+            initText2.textContent = 'Wait for Player One to begin';
             document.querySelector('.textTwoBoard').appendChild(initText2);
             const initScore1 = document.createElement('h1');
             initScore1.textContent = 'Score: 0';
@@ -14,7 +14,6 @@ var face1,face2,flag=0,point1,point2,opoint1=0,opoint2=0;
             document.querySelector('.scoreTwoBoard').appendChild(initScore2);
         }
         const playerOne = function (ch,board) {
-            var point;
             var playerEl;
             var boardEl,buttonEl1,buttonEl2;
             if(board === 1) {
@@ -45,17 +44,16 @@ var face1,face2,flag=0,point1,point2,opoint1=0,opoint2=0;
                         buttonEl2.style.display = "none";
                         document.getElementById('restart').style.visibility = "visible";;
                         return ch;
-                default:point=ch; 
-                        playerEl = document.createElement('p')
+                default:playerEl = document.createElement('p')
                         playerEl.textContent = `Point = ${ch}.`
                         boardEl.appendChild(playerEl) 
                         buttonEl1.style.display = "none";
                         buttonEl2.style.display = "initial";
-                        return point;
+                        return ch;
             }
         }
-        const playerSubsequent = function (ch,point1,board) {
-            var point;
+        const playerSubsequent = function (ch,pnt,board) {
+            debugger
             var playerEl;
             var boardEl,buttonEl1, buttonEl2;
             if(board === 1) {
@@ -68,14 +66,16 @@ var face1,face2,flag=0,point1,point2,opoint1=0,opoint2=0;
                 buttonEl2 = document.getElementById('button1');
             }
             boardEl.innerHTML = ''
+            if(ch===pnt) {
+                playerEl = document.createElement('p')
+                playerEl.textContent = `You rolled ${ch} again. You won!`
+                boardEl.appendChild(playerEl)
+                buttonEl1.style.display = "none";
+                buttonEl2.style.display = "none";
+                document.getElementById('restart').style.visibility = "visible";
+                return ch;
+            }
             switch(ch) {
-                case point1:playerEl = document.createElement('p')
-                            playerEl.textContent = `You rolled ${ch}. You won!`
-                            boardEl.appendChild(playerEl)
-                            buttonEl1.style.display = "none";
-                            buttonEl2.style.display = "none";
-                            document.getElementById('restart').style.visibility = "visible";
-                            return ch;
                 case 7:
                 case 11:playerEl = document.createElement('p')
                         playerEl.textContent = `You rolled ${ch}. You lost!`
@@ -84,13 +84,12 @@ var face1,face2,flag=0,point1,point2,opoint1=0,opoint2=0;
                         buttonEl2.style.display = "none";
                         document.getElementById('restart').style.visibility = "visible";
                         return ch;
-                default:point=ch; 
-                        playerEl = document.createElement('p')
+                default:playerEl = document.createElement('p')
                         playerEl.textContent = `Point = ${ch}.`
                         boardEl.appendChild(playerEl) 
                         buttonEl1.style.display = "none";
                         buttonEl2.style.display = "initial";
-                        return point;
+                        return ch;
             }
         }
 document.querySelector('#button1').addEventListener('click',function () {
@@ -113,10 +112,10 @@ document.querySelector('#button2').addEventListener('click',function () {
     face1=Math.floor(1+Math.random()*6);
     face2=Math.floor(1+Math.random()*6);
     ch=face1+face2;
-    if(flag==1) {
-        point2 = playerOne(ch,2);
+    if(flag!==1) {
+        point2 = playerSubsequent(ch,point2,2);
     } else {
-        point2 = playerSubsequent(ch,point1,2);
+        point2 = playerOne(ch,2);
         flag++;
     }
     opoint2+=point2;
